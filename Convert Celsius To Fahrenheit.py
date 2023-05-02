5cee996376b6ceeb06bfb15fa73e909e5c24be3a
RiyaaaSingh <114330695+RiyaaaSingh@users.noreply.github.com>
2023-05-02 22:27:19 +0530

    Create Convert Celsius To Fahrenheit.py

---
+++ Convert Celsius To Fahrenheit.py
@@ -0,0 +1,8 @@
+celsius = 37.5
+
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+
+
+Output
+37.5 degree Celsius is equal to 99.5 degree Fahrenheit
